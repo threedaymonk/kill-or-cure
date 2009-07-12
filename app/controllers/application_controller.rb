8c906e53b0e8ad213d2f2af2881c42773f99f81b
Paul Battley <pbattley@gmail.com>
2009-07-13 00:23:07 +0100

    Added a percent indicator.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  before_filter :load_percent_complete
+
+  def load_percent_complete
+    @percent_complete = [(Vote.count * 100) / (Article.count * 3), 100].min
+  end
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
