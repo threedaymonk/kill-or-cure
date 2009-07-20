7e3ae76b1670cf93fb9980600b7ae0972a880605
Paul Battley <pbattley@gmail.com>
2009-07-20 01:06:54 +0100

    Move the results to the homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.homepage   "/",        :controller => "home",    :action => "index"
+  map.homepage   "/about",   :controller => "home",    :action => "index"
   map.stats      "/stats",   :controller => "stats",   :action => "index"
-  map.results    "/results", :controller => "results", :action => "index"
+  map.results    "/",        :controller => "results", :action => "index"
   map.done       "/done",    :controller => "votes",   :action => "done"
   map.resources :votes
   map.resources :corrections
