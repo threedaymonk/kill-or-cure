d12165614e07cf2d62ed4caa4f02c06e44242055
Paul Battley <pbattley@gmail.com>
2009-07-23 10:59:00 +0100

    Paginate by letter. It's more useful.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,17 @@
 class ResultsController < ApplicationController
   def index
-    @terms = NormalizedTerm.paginate(
-               :page    => params[:page],
-               :order   => "LOWER(normalized_terms.value) ASC",
-               :include => [:articles])
+    @letters = NormalizedTerm.connection.select_all("SELECT DISTINCT LOWER(SUBSTR(value, 1, 1)) AS letter FROM normalized_terms").map{ |r| r["letter"] }.sort
+    if params[:page]
+      @terms = NormalizedTerm.paginate(
+                 :page    => params[:page],
+                 :order   => "LOWER(normalized_terms.value) ASC",
+                 :include => [:articles])
+    else
+      @letter = ((params[:letter] || '')[/^./] || 'a').downcase
+      @terms = NormalizedTerm.all(
+                 :conditions => ["normalized_terms.value LIKE ? OR normalized_terms.value LIKE ?", @letter + "%", @letter.upcase + "%"],
+                 :order   => "LOWER(normalized_terms.value) ASC",
+                 :include => [:articles])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.homepage   "/about",   :controller => "home",    :action => "index"
   map.stats      "/stats",   :controller => "stats",   :action => "index"
-  map.results    "/",        :controller => "results", :action => "index"
+  map.results    "/",            :controller => "results", :action => "index"
+  map.results    "/a-z/:letter", :controller => "results", :action => "index"
   map.done       "/done",    :controller => "votes",   :action => "done"
   map.resources :votes
   map.resources :corrections
